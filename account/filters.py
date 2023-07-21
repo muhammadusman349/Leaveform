3dafadb7ec4f84bf90c59b079de526bcd00c54bd
muhammadusman349 <usmankhadim.uk.uk@gmail.com>
2023-07-21 10:34:09 +0500

    add fields in filters

--- account/filters.py
+++ account/filters.py
@@ -2,12 +2,20 @@ import django_filters
 from .models import User,Department
 
 class UserFilter(django_filters.FilterSet):
+    first_name = django_filters.CharFilter(field_name="First Name",lookup_expr="icontains")
+    last_name = django_filters.CharFilter(field_name="Last Name",lookup_expr="icontains")
+    email = django_filters.CharFilter(field_name="Email",lookup_expr="icontains")
+    organization = django_filters.CharFilter(field_name="Organization",lookup_expr="exact")
+    department = django_filters.CharFilter(field_name="Department",lookup_expr="exact")
+
     class Meta:
         model = User
         fields= ["first_name","last_name","email","organization","department","created_at"]
 
 
 class DepartmentFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(field_name="Name",lookup_expr="icontains")
+    manager = django_filters.CharFilter(field_name="Manager",lookup_expr="exact")
     class Meta:
         model = Department
         fields= ["name","manager","created_at"]
--- leaveform/filters.py
+++ leaveform/filters.py
@@ -2,6 +2,12 @@ import django_filters
 from .models import LeaveForm
 
 class LeaveFormFilter(django_filters.FilterSet):
+    leave_type = django_filters.CharFilter(field_name="leave_type",lookup_expr="iexact")
+    type_of_request = django_filters.CharFilter(field_name="type_of_request",lookup_expr="iexact")
+    status = django_filters.CharFilter(field_name="status",lookup_expr="iexact")
+    start_date= django_filters.CharFilter(field_name="start_date",lookup_expr="gte")
+    end_date= django_filters.CharFilter(field_name="end_date",lookup_expr="lte")
+
     class Meta:
         model = LeaveForm
-        fields =["leave_type","type_of_request","start_date","end_date","status"]
+        fields =["leave_type","type_of_request","status","start_date","end_date"]
